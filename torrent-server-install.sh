11406fb2cb2641939e0715b3ba5ed307624e474d
cantalupo555 <cantalupo@protonmail.com>
2018-07-10 13:18:36 -0300

    Update

--- torrent-server-install.sh
+++ torrent-server-install.sh
@@ -16,7 +16,7 @@ sudo apt-get autoremove -y
 sudo apt-get install software-properties-common -y
 sudo add-apt-repository ppa:qbittorrent-team/qbittorrent-stable -y
 sudo apt-get update
-sudo apt-get install apache2 curl php libapache2-mod-php php-mcrypt php-mysql php7.0-zip php-curl php-gd php-mbstring php-mcrypt php-xml php-xmlrpc rtorrent proftpd qbittorrent qbittorrent-nox screen bmon htop --allow-unauthenticated -y
+sudo apt-get install proftpd apache2 curl php libapache2-mod-php php-mcrypt php-mysql php7.0-zip php-curl php-gd php-mbstring php-mcrypt php-xml php-xmlrpc rtorrent qbittorrent qbittorrent-nox screen bmon htop --allow-unauthenticated -y
 
 # Config Web
 sudo apache2ctl configtest
@@ -42,6 +42,7 @@ mkdir /home/rtorrent/.session
 #sudo adduser downloads --home=/home/rtorrent/Downloads --shell=/bin/false
 sudo useradd -m $user --home=/home/rtorrent --shell=/bin/false
 echo $user:$pass | chpasswd
+chown $user:$user /home/rtorrent
 
 # Config proFTPd
 cd /etc/proftpd/
