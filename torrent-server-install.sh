f66d6ff836f8b66223496841a217e2cdf1195318
cantalupo555 <cantalupo@protonmail.com>
2018-07-10 10:41:17 -0300

    Update

--- torrent-server-install.sh
+++ torrent-server-install.sh
@@ -80,19 +80,16 @@ chown -R 33:33 html/
 
 # Daemon
 cd ~
-echo -e "[Unit]\nDescription=qBittorrent Daemon Service\nAfter=network.target\n\n[Service]\nUser=$user\nExecStart=/usr/bin/qbittorrent-nox -d\nExecStop=/usr/bin/killall -w qbittorrent-nox\n\n[Install]\nWantedBy=multi-user.target"| sudo tee /etc/systemd/system/qbittorrent.service
+echo -e "[Unit]\nDescription=qBittorrent Daemon Service\nAfter=network.target\n\n[Service]\nUser=$user\nExecStart=/usr/bin/qbittorrent-nox\nExecStop=/usr/bin/killall -w qbittorrent-nox\n\n[Install]\nWantedBy=multi-user.target"| sudo tee /etc/systemd/system/qbittorrent.service
 echo -e "[Unit]\nDescription=rTorrent Daemon Service\nAfter=network.target\n\n[Service]\nUser=$user\nExecStart=/usr/bin/screen -d -m -S rtorrent /usr/bin/rtorrent\n#ExecStop=/usr/bin/screen -X -S rtorrent quit\n\n[Install]\n WantedBy=multi-user.target"| sudo tee /etc/systemd/system/rtorrent.service
 sudo systemctl daemon-reload && sudo systemctl enable qbittorrent && sudo systemctl start qbittorrent && sudo systemctl enable rtorrent && sudo systemctl start rtorrent
 echo -e '#! /bin/sh\n\n### BEGIN INIT INFO\n# Provides:           myService\n# Required-Start:     $local_fs $remote_fs $network $syslog $netdaemons\n# Required-Stop:      $local_fs $remote_fs\n# Default-Start:      2 3 4 5\n# Default-Stop:       0 1 6\n# Short-Description:  Example of init service.\n# Description:\n#  Long description of my service.\n### END INIT INFO\n\n# Actions provided to make it LSB-compliant\ncase "$1" in\n  start)\n    echo "Starting myService"\n    sudo /usr/bin/screen -d -m -S rtorrent /usr/bin/rtorrent\n    ;;\n  stop)\n    echo "Stopping script myService"\n    sudo /usr/bin/screen -X -S rtorrent quit\n    ;;\n  restart)\n    echo "Restarting script myService"\n    sudo /usr/bin/screen -X -S rtorrent quit && sudo /usr/bin/screen -d -m -S rtorrent /usr/bin/rtorrent\n    ;;\n  force-reload)\n    echo "Reloading script myService"\n    #Insert your reload routine here\n    ;;\n  status)\n    echo "Status of script myService"\n    #Insert your stop routine here\n    ;;\n  *)\n    echo "Usage: /etc/init.d/myService {start|stop|restart|force-reload|status}"\n    exit 1\n    ;;\nesac\n\nexit 0'| sudo tee /etc/init.d/myService
 sudo chmod +x /etc/init.d/myService && sudo chmod 777 /etc/init.d/myService && update-rc.d myService defaults
 clear
 echo "Installation Complete" && echo "By: @cantalupo555" && echo ""
-echo "Reboot seconds..."
+echo "Reboot..."
+echo "	Reboot..."
+echo "		Reboot..."
+echo "			Reboot..."
+echo "				Reboot..."
 sudo shutdown -r now
-
-#nano /home/rtorrent/Downloads/.config/qBittorrent/qBittorrent.conf
-#[Preferences]
-#Downloads\SavePath=/home/rtorrent/Downloads/
-#Queueing\QueueingEnabled=false
-#WebUI\Password_ha1=@ByteArray(e10adc3949ba59abbe56e057f20f883e)
-#WebUI\Username=$user
