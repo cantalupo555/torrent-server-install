da61833d5b071176f9bda4122b8c44269f889db7
cantalupo555 <cantalupo@protonmail.com>
2018-07-11 11:12:11 -0300

    Update

--- torrent-server-install.sh
+++ torrent-server-install.sh
@@ -11,6 +11,33 @@ if [ $UID -ne 0 ]; then
     exit 1
 fi
 
+# Ensure the OS is compatible with the launcher
+echo -e "\nChecking that minimal requirements are ok"
+if [ -f /etc/centos-release ]; then
+    OS="CentOs"
+    VERFULL=$(sed 's/^.*release //;s/ (Fin.*$//' /etc/centos-release)
+    VER=${VERFULL:0:1} # return 6 or 7
+elif [ -f /etc/lsb-release ]; then
+    OS=$(grep DISTRIB_ID /etc/lsb-release | sed 's/^.*=//')
+    VER=$(grep DISTRIB_RELEASE /etc/lsb-release | sed 's/^.*=//')
+elif [ -f /etc/os-release ]; then
+    OS=$(grep -w ID /etc/os-release | sed 's/^.*=//')
+    VER=$(grep VERSION_ID /etc/os-release | sed 's/^.*"\(.*\)"/\1/')
+ else
+    OS=$(uname -s)
+    VER=$(uname -r)
+fi
+ARCH=$(uname -m)
+
+echo "Detected : $OS  $VER  $ARCH"
+
+if [[ "$OS" = "Ubuntu" && ("$VER" = "16.04" || "$VER" = "18.04" ) ]] ; then
+    echo "Ok."
+else
+    echo "Sorry, this OS is not supported." 
+    exit 1
+fi
+
 # User e Password
 echo ""
 echo "@cantalupo555"
